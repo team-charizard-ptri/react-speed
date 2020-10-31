cc279eff46d17e042920c350b386d5d132bafa61
Matt Digel <med5342@gmail.com>
2020-10-31 12:06:05 -0700

    feed sending interaction data to the DB

--- src/Utils/firebaseDBrequests.js
+++ src/Utils/firebaseDBrequests.js
@@ -3,7 +3,12 @@ import firestore from '@react-native-firebase/firestore';
 
 const ref = firestore().collection('interactions');
 
-export const createInteraction = () => {
+export const createInteraction = ({
+  photoID,
+  firstClick_Coordinates,
+  secondClick_Coordinates,
+  reactionTime,
+}) => {
   auth().onAuthStateChanged(userFirebase => {
     if (userFirebase) {
       console.log('user id', userFirebase.uid);
@@ -11,23 +16,16 @@ export const createInteraction = () => {
       const newInteraction = {
         // userID: userFirebase.uid,
         userID: userFirebase.uid,
-        photoID:
-          'https://www.coats.com/-/media/Coats/End-Use/Household-and-Recreation/Kites/Household-and-recreation---sporting-goods--kite-01.jpg?rev=0ba06cc34a384b8aa9bf5afcb85472f7&width=600&height=338&op=crop',
-        firstClick_Coordinates: {
-          x: 234,
-          y: 435,
-        },
-        secondClick_Coordinates: {
-          x: 123,
-          y: 543,
-        },
-        reactionTime: 5.5,
+        photoID: photoID,
+        firstClick_Coordinates: firstClick_Coordinates,
+        secondClick_Coordinates: secondClick_Coordinates,
+        reactionTime: reactionTime,
       };
 
       ref
         .add(newInteraction)
         .then(() => {
-          console.log('Test successful!');
+          // console.log('Success sending this:', newInteraction);
         })
         .catch(err => console.log('error in add test', err));
     }
@@ -40,13 +38,12 @@ export const getReactionTimesForUser = () => {
       if (userUIFirebase) {
         const interactions = await firestore()
           .collection('interactions')
-          // .doc('LsnQkIw9pzY4JpUuYb7K')
           .where('userID', '==', userUIFirebase.uid)
           .get();
         const reactTimes = interactions._docs.map(
           document => document._data.reactionTime,
         );
-        console.log(reactTimes);
+        // console.log(reactTimes);
       }
     } catch (e) {
       console.log('error', e);
